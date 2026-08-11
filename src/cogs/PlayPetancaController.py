3d3756c03c86a5d097275f2f0f48887cbd3286a1
Michael Suárez <onriosrz@gmail.com>
2026-08-11 20:27:55 +0200

    añadiendo respuesta extra

--- src/cogs/PlayPetancaController.py
+++ src/cogs/PlayPetancaController.py
@@ -1,11 +1,19 @@
 import datetime
 from zoneinfo import ZoneInfo
 import asyncio
+import random
 from discord.ext import commands, tasks
 class PlayPetancaController(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.petanca_message.start() #Como esto ya no es un evento que espera a que alguien ponga un comando, debes indicarle que empiece.
+        self.messages = [
+            "Ignorada bastisima...",
+            "Racismo.",
+            "Tu #%TA Madre bot de petanca!",
+            "Vivimos en una sociedad...",
+            "Sálganse del chat, quiero estar solo :("
+        ]
 
     @tasks.loop(time=datetime.time(hour=20, minute=20, tzinfo=ZoneInfo("Europe/Madrid")))
     async def petanca_message(self):
@@ -14,9 +22,13 @@ class PlayPetancaController(commands.Cog):
         if canal is None:
             print("No he encontrado Petanca")
             return
+        message = random.choice(self.messages)
         await canal.send("hora de jugar a la petanca!")
         await asyncio.sleep(2)
         await canal.send("!petanqueo")
+        await asyncio.sleep(2)
+        await canal.send(message)
+        
 
 
 async def setup(bot): #esta función es la que busca el hook.
