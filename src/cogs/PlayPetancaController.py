621a55dfb99f02a64eb951f514a38ff764a91606
Michael Suárez <onriosrz@gmail.com>
2026-08-11 20:04:28 +0200

    editando la hora

--- src/cogs/PlayPetancaController.py
+++ src/cogs/PlayPetancaController.py
@@ -6,7 +6,7 @@ class PlayPetancaController(commands.Cog):
         self.bot = bot
         self.petanca_message.start() #Como esto ya no es un evento que espera a que alguien ponga un comando, debes indicarle que empiece.
 
-    @tasks.loop(time=datetime.time(hour=20, minute=00, tzinfo=ZoneInfo("Europe/Madrid")))
+    @tasks.loop(time=datetime.time(hour=20, minute=10, tzinfo=ZoneInfo("Europe/Madrid")))
     async def petanca_message(self):
         print("se ha ejecutada petanca")
         canal = self.bot.get_channel(776505761836171315) #Id del canal
